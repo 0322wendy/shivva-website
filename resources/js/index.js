2c45644914f4bbaefda6992c4f4b0b3f1d48fda9
0322wendy <w.g.orellana@gmail.com>
2022-08-04 17:56:44 -0400

    fixed everything on the list EXCEPT hamburger menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,6 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
+  var navContainer = document.getElementsByClassName("header-container");
   var links = document.getElementById("links");
   var nav = document.getElementById("nav");
   var navLinks = document.getElementById("nav-links");
@@ -9,14 +10,36 @@ function myFunction() {
   } else {
     links.style.display = "block";
     // Styling navigation
-    nav.style.backgroundColor = "#333";
+    nav.style.backgroundColor = "rgba(8, 8, 12, 0.95)";
     nav.style.position = "absolute";
     nav.style.top = "84px";
     // Styling links
-    navLinks.style.padding = "14px 16px";
+    navLinks.style.padding = "16px 48px";
     navLinks.style.display = "block";
+    navLinks.style.width = "100%";
+    navLinks.style.margin = "0 auto";
 
     navListItem.style.float = "none";
     navListItem.style.width = "100%";
+
+    navContainer.style.backgroundColor = "rgba(8, 8, 12, 0.95)";
   }
 }
+
+/* Fade in effect */
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
